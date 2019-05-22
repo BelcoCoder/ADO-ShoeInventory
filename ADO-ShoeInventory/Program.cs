d8d0c9a1d4b15e9b22434823928cbbb7237ab39e
berqhua <besikciberkcan@yahoo.com>
2019-05-22 03:33:12 +0300

    Create

--- ADO-ShoeInventory/Program.cs
+++ ADO-ShoeInventory/Program.cs
@@ -16,7 +16,7 @@ namespace ADO_ShoeInventory
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
